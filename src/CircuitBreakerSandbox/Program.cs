a9b2cb65c6d68af0994225979bb7903f635710d3
Felipe Barbirato <felipe.barbirato@albumprinter.com>
2016-11-28 17:39:54 +0100

    Initial commit

---
+++ src/CircuitBreakerSandbox/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Polly;
+
+namespace CircuitBreakerSandbox
+{
+    class Program
+    {
+        private static int _count = 0;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                GetDataFromCache();
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+
+        }
+
+        private static void GetDataFromCache()
+        {
+            var policy = Policy
+                .Handle<Exception>()
+                //.Retry(3);
+                .WaitAndRetry(2, i => TimeSpan.FromSeconds(5));
+
+            Action<Exception, TimeSpan, Context> onBreak = (exception, timespan, context) =>
+            {
+                // turn off redis
+            };
+            Action<Context> onReset = context =>
+            {
+
+            };
+
+            var breaker = Policy
+                .Handle<Exception>()
+                .CircuitBreaker(1, TimeSpan.FromMinutes(2), onBreak, onReset);
+
+            breaker.Execute(GetItem);
+        }
+
+        private static void GetItem()
+        {
+            throw new Exception("Redis not available...");
+        }
+    }
+}
